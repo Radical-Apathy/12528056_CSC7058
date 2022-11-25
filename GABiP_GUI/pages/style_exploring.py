029bc0d067c5f16176269ecc23044b70161bc7c7
Radical-Apathy <radical_apathy@outlook.com>
2022-11-25 15:34:40 +0000

    exploring css styling

--- GABiP_GUI/pages/style_exploring.py
+++ GABiP_GUI/pages/style_exploring.py
@@ -10,75 +10,38 @@ def data_upload():
     df =pd.read_csv('C:/Users/Littl/OneDrive/Desktop/dataset_clean _towrite.csv', encoding= 'unicode_escape', low_memory=False)
     return df
 
+
+def css_file(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+#st.session_state
+
+
+css_file("C:/Users/Littl/OneDrive/Documents/GitHub/12528056_CSC7058/GABiP_GUI/pages/style/style.css")
+st.header("Style Exploring")
+
 #st.header("This is Streamlit Default Dataframe")
-df = data_upload()
+dfToEdit = data_upload()
 # st.dataframe(data=df)
 # st.info(len(df))
 
-_funct = st.sidebar.radio(label="Functions", options = ['Display','Highlight','Delete'])
+st.write("HTML injected form")
 
-st.header("This is AgGrid Table")
 
-gd = GridOptionsBuilder.from_dataframe(df)
-gd.configure_pagination(enabled=True)
-gd.configure_default_column(editable=True,groupable=True)
-# _______________________________________________________________
-# Enabling tooltip - YouTube-Query by Alexis-Raja Brachet  
-# gd.configure_default_column(editable=True,groupable=True,tooltipField = "variant") 
-# hover in any rows( under any columns), the variant of that row, will pop up as tootltip information.However, 
-# I'm yet to figure out, how to implement more than one column information (what I mean - let's say - ["variant", "date"] collectively as tooltip information) , 
-# also, it's bit slow in the begginig  when I tested. 
-# ________________________________________________________________
-
-if _funct == 'Display':
-    sel_mode = st.radio('Selection Type', options = ['single', 'multiple'])
-    gd.configure_selection(selection_mode=sel_mode,use_checkbox=True)
-    gridoptions = gd.build()
-    grid_table = AgGrid(df,gridOptions=gridoptions,
-                        update_mode= GridUpdateMode.SELECTION_CHANGED,
-                        height = 500,
-                        allow_unsafe_jscode=True,
-                        #enable_enterprise_modules = True,
-                        theme = 'fresh')
-
-    sel_row = grid_table["selected_rows"]
-    st.subheader("Output")
-    st.write(sel_row)
-if _funct == 'Highlight':
-    col_opt = st.selectbox(label ='Select column',options = df.columns)
-    cellstyle_jscode = JsCode("""
-        function(params){
-            if (params.value == 'Allophrynidae') {
-                return {
-                    'color': 'black',
-                    'backgroundColor' : 'orange'
-            }
-            }
-            if (params.value == 'B.1.258') {
-                return{
-                    'color'  : 'black',
-                    'backgroundColor' : 'red'
-                }
-            }
-            else{
-                return{
-                    'color': 'black',
-                    'backgroundColor': 'lightpink'
-                }
-            }
-       
-    };
+html_form=("""<form action="google.com" method="POST">
+    <h3 id = "header"><strong>Order<strong></h3>
+    <input type="text" name="name"  placeholder="Order - e.g. Anura" required>
+   <h3 id = "header"><strong>Family<strong></h3>
+    <input type="text" name="name"  placeholder="Family - e.g. Allophrynidae" required>
+    <h3 id = "header"><strong>Genus<strong></h3>
+    <input type="text" name="name"  placeholder="Genus - e.g. Allophryne" required>
+    <h3 id = "header"><strong>Species<strong></h3>
+    <input type="text" name="name"  placeholder="Species - e.g. Relicta" required>
+    <button type="submit">Send</button>
+    </form> 
     """)
-    gd.configure_columns(col_opt, cellStyle=cellstyle_jscode)
-    gridOptions = gd.build()
-    grid_table = AgGrid(df, 
-            gridOptions = gridOptions, 
-            enable_enterprise_modules = True,
-            fit_columns_on_grid_load = True,
-            height=500,
-            width='100%',
-            #theme = "material",
-            update_mode = GridUpdateMode.SELECTION_CHANGED,
-            reload_data = True,
-            allow_unsafe_jscode=True,
-            )
+st.markdown(html_form, unsafe_allow_html=True)
+
+
+st.write("Streamlit version")
