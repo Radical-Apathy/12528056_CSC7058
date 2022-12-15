2e1990b617836131caf9a1fdd337d55608e983b5
Radical-Apathy <radical_apathy@outlook.com>
2022-12-15 13:19:47 +0000

    adding pending users to admin page

--- GABiP_GUI/pages/05_admin_page_dev.py
+++ GABiP_GUI/pages/05_admin_page_dev.py
@@ -9,26 +9,67 @@ from deta import Deta
 import os
 from dotenv import load_dotenv
 
-#-----------------database connection and method to insert a user-----------------------------#
+#------------------------------------------------------------DATABASE CONNECTION-----------------------------------------------------------------------------------------#
 load_dotenv("C:/Users/Littl/OneDrive/Documents/GitHub/12528056_CSC7058\GABiP_GUI/.env.txt")
 deta_key=os.getenv("deta_key")
-
 
 #initialising a deta object
 deta_connection= Deta(deta_key)
 
 db=deta_connection.Base("users_db")
 
-#--------------------------------UI display methods------------------------------------------#
+#--------------------------------------------------------------DATABASE METHODS------------------------------------------------------------------------------------------------------------------------#
+
+#returns a dictionary of all users
+def get_all_users():
+    res = db.fetch()
+    return res.items
+
+#converts each individual values for users to a their own list using list comprehension
+users=get_all_users()
+email=[user["key"] for user in users]
+username=[user["username"] for user in users]
+firstname=[user["firstname"] for user in users]
+surname = [user["surname"] for user in users]
+hashed_passwords=[user ["password"] for user in users]
+isApproved=[user["approved"]for user in users]
+isAdmin=[user["admin"] for user in users]
+
+#updates user to approved. Action attached to checkbox in display_pending_users()
+def approve_user(username, updates):
+    return db.update(updates, username)
+
+
+
+#gets and displays users pending approval
+def display_pending_users():
+    st.markdown("***")
+    for user in users:
+     if user["approved"]=="False":
+       with st.form(user["username"]):
+        st.markdown(f"""<p style="font-family:sans-serif; color:ForestGreen; font-size: 20px;"><strong>***********{user["username"]}'s Request**********</strong></p>""" , unsafe_allow_html=True)
+        st.text(f"Username : " +user["username"])
+        st.text(f"Firstname : " +user["firstname"])
+        st.text(f"Surname : " +user["surname"])
+        st.text(f"Email : " +user["key"])
+        checkbox1 = st.checkbox(f"Allow " + user["firstname"] + " access")
+        checkbox2 = st.checkbox(f"Place " +user["firstname"] +" in review list")
+        confirmForm = st.form_submit_button(f"Submit Decision for  : " + user["username"])
+        if checkbox1 and checkbox2 and confirmForm:
+            st.error("Warning! Both options have been selected. Please review decision")
+        elif checkbox1 and confirmForm:
+            approve_user(user["key"], updates={"approved": "True"})
+            st.success(f"Accepted! "+user["username"]+ " can now access the GABiP. You can revoke access at any time using the View Approved user's option")
+        elif checkbox2 and confirmForm:
+            st.warning(f"User now place in to review section. " +user["username"]+ " 's access can be decided upon another date" )
+      
+        st.markdown("""<p style="font-family:sans-serif; color:ForestGreen; font-size: 20px;"><strong>**************************************************************************************</strong></p>""", unsafe_allow_html=True )
+        st.write("***")
+
+
+#------------------------------------------------------------------ADMIN DISPLAY METHODS---------------------------------------------------------------------------------------#
 def welcome_screen():
     st.image("amphibs.jpeg", width=200)
-
-def view_approved_users():
-    st.write("This is a blank page")
-
-
-
-
 
 
 #-----------------------------------------st title-------------------------------------------#
@@ -36,11 +77,11 @@ def view_approved_users():
 st.header("**************************:lock:Admin Section:lock_with_ink_pen:**************************")
 st.subheader("Welcome to the Admin Area.")
 
-adminOptions= st.selectbox("Options", ['Click here to see Admin options','View pending users', 'View approved users','See pending changes'  ])
+adminOptions= st.selectbox("Options", ['Click here to see Admin options','View Access Requests', 'View approved users','See pending changes'  ])
 if adminOptions=="Click here to see Admin options":
     welcome_screen()
-if adminOptions=="View pending users":
-    view_approved_users()
+if adminOptions=="View Access Requests":
+    display_pending_users()
 
 st.markdown("***")
      
