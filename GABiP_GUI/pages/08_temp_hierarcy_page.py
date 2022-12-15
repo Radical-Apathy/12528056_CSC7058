276bcc55afc47c3cc45861d0c24bcefa4ad602f7
Radical-Apathy <radical_apathy@outlook.com>
2022-12-15 10:44:55 +0000

    exploring file hierarchy
    putting aside for now and moving on

--- GABiP_GUI/pages/08_temp_hierarcy_page.py
+++ GABiP_GUI/pages/08_temp_hierarcy_page.py
@@ -2,13 +2,7 @@ from pathlib import Path
 import sys
 #sys.path.append(str(Path('C:/Users/Littl/OneDrive/Documents/GitHub/12528056_CSC7058/GABiP_GUI').resolve().parent.parent))
 #sys.path.append("C:/Users/Littl/OneDrive/Documents/GitHub/12528056_CSC7058"/GABiP_GUI/methods_main")
-sys.path.append(str(Path("C:/Users/Littl/OneDrive/Documents/GitHub/12528056_CSC7058/GABiP_GUI/methods_main").resolve().parent.parent))
-#from methods_main import hello 
+sys.path.insert(1,"C:/Users/Littl/OneDrive/Documents/GitHub/12528056_CSC7058/GABiP_GUI/pages/methods_pages" )
 import streamlit as st
-#h.hello_terminal()
-#from methods import hello as h
-
-#hello.streamlit_hello()
-
-#st.write(sys.path)
+from methods_pages import hello
 
