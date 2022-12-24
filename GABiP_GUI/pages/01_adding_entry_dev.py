51d457cd640ce95959432635a34e9ca59496abe4
Radical-Apathy <radical_apathy@outlook.com>
2022-12-24 19:38:14 +0000

    developing adding entry page

--- GABiP_GUI/pages/01_adding_entry_dev.py
+++ GABiP_GUI/pages/01_adding_entry_dev.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 from deta import Deta
 import os
+import csv
 from dotenv import load_dotenv
 from datetime import datetime
 st.set_page_config(page_icon='amphibs.jpeg')
@@ -57,7 +58,7 @@ def add_changes(dataframe, dataframe2):
     return updated
 
 
-def create_new_csv(dataframe, path):
+def create_new_csv(new_dataframe, path):
     new_dataframe.to_csv(path,index=False)
 
 path=get_latest()
@@ -72,42 +73,179 @@ current_db=load_latest()
 
 
 
+
+
+
+#------------------------------------------------------------SESSION STATE VALUES-----------------------------------------------------------------------------------------#
+#if 'SVLMMx' not in st.session_state:
+ #   st.session_state['SVLMMx'] = "SVLMMx"
+
+st.session_state
+
 #------------------------------------------------------------MAIN PAGE-----------------------------------------------------------------------------------------#
 st.header('Add Entry page')
 
-
 st.write(current_db)
-
+#changed=pd.read_csv("C:/Users/Littl/OneDrive/Documents/GitHub/12528056_CSC7058/GABiP_GUI/pages/GABiP_Databases/changesfromaddpage.csv",encoding= 'unicode_escape', low_memory=False)
 path_prefix="C:/Users/Littl/OneDrive/Documents/GitHub/12528056_CSC7058/GABiP_GUI/pages/GABiP_Databases/"
+dbColumns=current_db.columns
+#st.write(changed)
+#style_template = '<p style="font-family:sans-serif; color:Green; font-size: 42px;"><strong>More Options</strong></p>'
+
+#columns: SVLMMx, SVLFMx, SVLMx, Longevity, NestingSite, ClutchMin,	ClutchMax,	Clutch,	ParityMode,	EggDiameter,	
+# Activity,	Microhabitat, GeographicRegion,	IUCN,	PopTrend,	RangeSize	ElevationMin	ElevationMax	Elevation	
+
+def create_session_states(dbColumns):
+    for column in dbColumns:
+        if column not in st.session_state:
+           st.session_state[column] =""
+
+        #st.session_state[column] == column
+        
+
+create_session_states(dbColumns)
+#for column in dbColumns:
+ #   st.write(column)
+
+
+#create_session_states(dbColumns)
+
+#add entry using form
+
+#with st.form("my_form"):
+#          st.markdown('<p style="font-family:sans-serif; color:Green; font-size: 30px;"><strong>***       Mandatory Fields         ***</strong></p>', unsafe_allow_html=True)
+#          order =st.text_input("Order","Order - e.g. Anura", key='Order')
+#          family =st.text_input("Family","Family - e.g. Allophrynidae", key='Family')
+#          genus =st.text_input("Genus", "Genus - e.g. Allophryne", key='Genus')
+#          species =st.text_input("Species","Species - e.g. Relicta", key='Species')
+#          submitted = st.form_submit_button("Show more options")
+#          if submitted:
+#            st.write(order, family, genus, species)
+
+userInfo=[]
+#without a form
+st.markdown('<p style="font-family:sans-serif; color:Green; font-size: 30px;"><strong>***       Mandatory Fields         ***</strong></p>', unsafe_allow_html=True)
+order =st.text_input("Order","Order - e.g. Anura", key='Order') 
+family =st.text_input("Family","Family - e.g. Allophrynidae", key='Family')
+genus =st.text_input("Genus", "Genus - e.g. Allophryne", key='Genus')
+species =st.text_input("Species","Species - e.g. Relicta", key='Species')
+
+
+#--------------------------------------------------------------------------------------------MANAGING ADDITIONAL FIELDS DEV-------------------------------------------------------#
+st.markdown('***')
+st.markdown('<p style="font-family:sans-serif; color:Green; font-size: 20px;"><strong>More Options</strong></p>', unsafe_allow_html=True)
+more_options=st.multiselect("Add more Information", ['SVLMMx', 'SVLFMx', 'SVLMx', 'Longevity', 'NestingSite', 'ClutchMin',	'ClutchMax',
+                             'Clutch', 'ParityMode',	'EggDiameter', 'Activity',	'Microhabitat', 'GeographicRegion',	'IUCN',	
+                             'PopTrend',	'RangeSize', 'ElevationMin','ElevationMax','Elevation'])
+# Activity,	Microhabitat, GeographicRegion,	IUCN,	PopTrend,	RangeSize	ElevationMin	ElevationMax	Elevation], label_visibility="visible")
+
+
+def display_extra_fields():
+    for option in more_options:
+        userText=st.text_input(option, key=option)
+        st.session_state[option] == userText
+
+def get_extra_userinfo():
+    for option in more_options:
+        
+        userText=st.text_input(option, key=option)
+        if userText:
+         st.session_state[option] == userText
+        elif not userText :
+            st.session_state[option]==""
+       # userInfo.append(userText)
 
 
 
 
-#add entry form
+if more_options:
+    get_extra_userinfo()
 
-with st.form("my_form"):
-          st.write("Inside the form")
-          order =st.text_input("Order","Order - e.g. Anura", key='Order')
-          family =st.text_input("Family","Family - e.g. Allophrynidae", key='Family')
-          genus =st.text_input("Genus", "Genus - e.g. Allophryne", key='Genus')
-          species =st.text_input("Species","Species - e.g. Relicta", key='Species')
-          submitted = st.form_submit_button("Submit")
-          if submitted:
-            st.write(order, family, genus, species)
+#st.write(current_db.columns)
+def populate_userinfo():
+    for column in dbColumns:
+        userInfo.append(st.session_state[column])
+    #st.write(userInfo)
+
+
+def construct_complete_dataframe(userinfo, dbcolumns):
+    completed = pd.DataFrame(userInfo, dbColumns)
+    st.write(completed) 
+
+def construct_complete_dataframe_columns(userinfo, columns=current_db.columns):
+    completed = pd.DataFrame(userInfo, current_db.columns)
+    st.write(completed) 
+
+
+
+def submit_changes():
+    populate_userinfo()
+    new_dataframe=pd.DataFrame(userInfo, dbColumns)
+    new_dataframe.to_csv("C:/Users/Littl/OneDrive/Documents/GitHub/12528056_CSC7058/GABiP_GUI/pages/GABiP_Databases/changesfromaddpage.csv")
+
+review_information=st.button("Review Information")
+
+#def add_changes(dataframe, dataframe2):
+#    updated=dataframe.append(dataframe2, ignore_index = True)
+#    return updated
+
+def create_list(dbcolumns, userInfo):
+    userdf=[]
+    for column in dbcolumns:
+        for value in userInfo:
+            data={column:value}
+            
+        userdf=pd.DataFrame(data, index=False)
+    
+    st.write(userdf)
+
+
+if review_information:
+    populate_userinfo()
+    create_list(current_db.columns, userInfo)
+    #construct_complete_dataframe_columns(userInfo, columns=current_db.columns)
+    
+
+commit_changes=st.button("Submit for review")
+
+if commit_changes:
+    #submit_changes()
+    st.write("Changes submitted")
+   
+    #st.success("Additons submitted for review")
+#changes=pd.read_csv("C:/Users/Littl/OneDrive/Documents/GitHub/12528056_CSC7058/GABiP_GUI/pages/GABiP_Databases/changesfromaddpage.csv", encoding= 'unicode_escape', low_memory=False)
+#st.write("You selected", more_options)
+
+ #def construct_complete_dataframe(userinfo, dbcolumns):
+#    completed = pd.DataFrame(userInfo, dbColumns)
+#    return completed
+
+
+#dropdown_button=st.button("Submit values")
+
+
+
+
+#    st.write(userInfo) 
+
+
+
+
+
+
+
 
 
 #new_dataframe=({"Order":[order], "Family":[family], "Genus":[genus], "Species":[species]})
-new_dataframe=({"Order":[order], "Family":[family], "Genus":[genus], "Species":[species]})
 
-new_dataframe=pd.DataFrame(new_dataframe)
-st.write("values added "+ new_dataframe)
-#st.write(new_dataframe)
-#st.write(new_dataframe)
-updatedDataframe=(add_changes(current_db, new_dataframe))
-st.write(updatedDataframe)
+#new_dataframe=pd.DataFrame(new_dataframe)
+#st.write("values added "+ new_dataframe)
 
-path_prefix="C:/Users/Littl/OneDrive/Documents/GitHub/12528056_CSC7058/GABiP_GUI/pages/GABiP_Databases/"
-now=str(datetime.now())
+#updatedDataframe=(add_changes(current_db, new_dataframe))
+#st.write(updatedDataframe)
+
+#path_prefix="C:/Users/Littl/OneDrive/Documents/GitHub/12528056_CSC7058/GABiP_GUI/pages/GABiP_Databases/"
+#now=str(datetime.now())
 #new_path=path_prefix+now+".csv"
 
 #st.write(new_path)
