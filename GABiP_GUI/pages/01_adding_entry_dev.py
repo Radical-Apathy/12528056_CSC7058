9cd3ebc3d6fa42efbb75ea2e550dfc9d6ba5a119
Radical-Apathy <radical_apathy@outlook.com>
2023-01-09 14:49:44 +0000

    tidying code

--- GABiP_GUI/pages/01_adding_entry_dev.py
+++ GABiP_GUI/pages/01_adding_entry_dev.py
@@ -15,7 +15,7 @@ deta_key=os.getenv("deta_key")
 #initialising a deta object
 deta_connection= Deta(deta_key)
 
-metaData=deta_connection.Base("database_versions")
+metaData=deta_connection.Base("database_metadata")
 
 
 #------------------------------------------------------------ DATABASE METHODS-----------------------------------------------------------------------------------------#
@@ -31,7 +31,7 @@ databases=get_all_paths()
 
 date_time= sorted([database["key"] for database in databases], reverse=True)
 status=[database["Status"] for database in databases]
-path = [database["File_Path"] for database in databases]
+path = [database["Current Dataset"] for database in databases]
 
 #getting the most recent approved csv file
 def get_latest():
@@ -40,12 +40,13 @@ def get_latest():
         
       if database["key"]== i and database["Status"] =="Approved":
         break
-    return(database["File_Path"])
+    return(database["Current Dataset"])
 
 #add user's entries to csv 
-def insert_csv(date_time, file_Path, edit_type, username, status):
+def add_to_database(date_time, changes_file_Path, dataset_pre_change, edit_type, species_affected, genus_affected, username, user_comment, status, reason_denied, approved_by, date_approved, current_database_path):
     """adding user"""
-    return metaData.put({"key":date_time, "File_Path": file_Path, "Edit_Type": edit_type, "Edited_By":username, "Status":status })
+    #defining the email as the key
+    return metaData.put({"key":date_time, "Changes": changes_file_Path, "Dataset_Pre_Change": dataset_pre_change, "Edit_Type": edit_type, "Species_Affected": species_affected, "Genus_Affected": genus_affected,"Edited_By":username,"User_Comment": user_comment, "Status":status, "Reason_Denied":reason_denied, "Approved_By":approved_by, "Date_Approved":date_approved, "Current Dataset":current_database_path })
 
 
 path=get_latest()
@@ -63,13 +64,61 @@ current_db=load_latest()
 if 'comment' not in st.session_state:
     st.session_state['comment']=""
 
+#creating session state variables for each column in dataset
 def create_session_states(dbColumns):
     for column in dbColumns:
         if column not in st.session_state:
            st.session_state[column] =""
          
-st.session_state['comment']
-#------------------------------------------------------------MAIN PAGE-----------------------------------------------------------------------------------------#
+#st.session_state["username"]
+
+#------------------------------------------------------------METHODS----------------------------------------------------------------------------------------------------------#
+
+#enforcing mandatory fields
+def blank_validation(states=['order','family','genus','species']):
+    for i in states:
+     if i=="":
+        st.warning("Order, Family, Genus, Species fields can not be left blank. Please recheck mandatory field section")
+
+#displays text fields for optional information
+def display_extra_fields():
+    for option in more_options:
+        userText=st.text_input(option, key=option)
+        st.session_state[option] == userText
+
+#sets session state values for optional extra fields
+def get_extra_userinfo():
+    for option in more_options:
+        
+        userText=st.text_input(option, key=option)
+        if userText:
+         st.session_state[option] == userText
+        elif not userText :
+            st.session_state[option]==""
+
+#stores user info in an array      
+def populate_userinfo():
+    for column in dbColumns:
+        userInfo.append(st.session_state[column])
+
+#checking that both the genus and species submitted don't exist on current csv    
+def check_current_db(genus, species):
+    if genus.lower() in current_db["Genus"].str.lower().values and species.lower() in current_db["Species"].str.lower().values:
+        st.warning(f"Data already exists for " +genus+ " " +species+ " Check full dataset option and consider making and edit to current dataset instead of an addition") 
+
+#contructs a dataframe for user to see summary of their addition
+def construct_review_dataframe(userinfo, columns=current_db.columns):
+    completed = pd.DataFrame(userInfo, current_db.columns)
+    st.write(completed) 
+
+#creates a csv file with users addition
+def create_csv(columnrow, inforow):
+    with open(newPath,  'w', encoding= 'UTF8', newline='') as f:
+        writer=csv.writer(f)
+        writer.writerow(columnrow)
+        writer.writerow(inforow)
+
+#------------------------------------------------------------MAIN PAGE--------------------------------------------------------------------------------------------------------#
 st.header('Add Entry page')
 
 st.write(current_db)
@@ -79,13 +128,8 @@ dbColumns=current_db.columns
 #creating session state variables for csv columns
 create_session_states(dbColumns)
 
-def blank_validation(states=['order','family','genus','species']):
-    for i in states:
-     if i=="":
-        st.warning("Order, Family, Genus, Species fields can not be left blank. Please recheck mandatory field section")
 
 userInfo=[]
-#without a form
 
 st.markdown('<p style="font-family:sans-serif; color:Green; font-size: 30px;"><strong>***      * Mandatory Fields *        ***</strong></p>', unsafe_allow_html=True)
 order =st.text_input("Order","Order - e.g. Anura", key='Order') 
@@ -105,67 +149,20 @@ more_options=st.multiselect("Add more Information", ['SVLMMx', 'SVLFMx', 'SVLMx'
                              'PopTrend',	'RangeSize', 'ElevationMin','ElevationMax','Elevation'])
 
 
-
-def display_extra_fields():
-    for option in more_options:
-        userText=st.text_input(option, key=option)
-        st.session_state[option] == userText
-    
-
-def get_extra_userinfo():
-    for option in more_options:
-        
-        userText=st.text_input(option, key=option)
-        if userText:
-         st.session_state[option] == userText
-        elif not userText :
-            st.session_state[option]==""
-      
-
-
 if more_options:
     get_extra_userinfo()
 
-#st.write(current_db.columns)
-def populate_userinfo():
-    for column in dbColumns:
-        userInfo.append(st.session_state[column])
+
    
-
-
-def construct_complete_dataframe(userinfo, dbcolumns):
-    completed = pd.DataFrame(userInfo, dbColumns)
-    st.write(completed) 
-
-def construct_complete_dataframe_columns(userinfo, columns=current_db.columns):
-    completed = pd.DataFrame(userInfo, current_db.columns)
-    st.write(completed) 
-
-
-
-def submit_changes():
-    populate_userinfo()
-    new_dataframe=pd.DataFrame(userInfo, columns=dbColumns)
-    
 review_information=st.button("Review Information")
-
-def add_changes(dataframe, dataframe2):
-    updated=dataframe.append(dataframe2, ignore_index = True)
-    return updated
-    
-#checking that both the genus and species submitted don't exist on current csv    
-def check_current_db(genus, species):
-    if genus.lower() in current_db["Genus"].str.lower().values and species.lower() in current_db["Species"].str.lower().values:
-        st.warning(f"Data already exists for " +genus+ " " +species+ " Check full dataset option and consider making and edit to current dataset instead of an addition") 
-   
-    
+       
 
 if review_information:
     
     populate_userinfo()
     blank_validation([st.session_state['Order'], st.session_state['Family'], st.session_state['Genus'], st.session_state['Species']])
     check_current_db(st.session_state['Genus'], st.session_state['Species']) 
-    userdf=construct_complete_dataframe_columns(userInfo, columns=current_db.columns)
+    userdf=construct_review_dataframe(userInfo, columns=current_db.columns)
     
 
 user_message=st.text_area("Please leave a comment citing the source for this addition", key='comment')
@@ -173,33 +170,20 @@ user_message=st.text_area("Please leave a comment citing the source for this add
 
 commit_changes=st.button("Submit for review")
 
-
-
-
 now=datetime.now()
-#
 timeStamp=now.strftime("%d.%m.%Y-%H.%M.%S")
-st.write(now)
-newPath=""
-def create_csv(columnrow, inforow):
-    path_prefix="C:/Users/Littl/OneDrive/Documents/GitHub/12528056_CSC7058/GABiP_GUI/pages/pending changes/"
-    path_end = timeStamp
-    newPath=path_prefix+path_end+"-"+st.session_state['username']+".csv"
- 
-    with open(newPath,  'w', encoding= 'UTF8', newline='') as f:
-        writer=csv.writer(f)
-        writer.writerow(columnrow)
-        writer.writerow(inforow)
-    return newPath
-        
+path_prefix="C:/Users/Littl/OneDrive/Documents/GitHub/12528056_CSC7058/GABiP_GUI/pages/pending changes/Additions/"
+path_end = timeStamp
+newPath=path_prefix+path_end+"-"+st.session_state['username']+".csv"
 
+        
 if commit_changes and user_message:
-    
     populate_userinfo()
     columnrow=current_db.columns
     inforow=userInfo
     create_csv(columnrow, inforow)
-    st.markdown("Changes submitted") 
+    #add_to_database(str(now), newPath, get_latest(), "Addition", st.session_state["Species"], st.session_state["Genus"], st.session_state["username"], st.session_state["comment"], "Pending", "n/a", "n/a", "n/a", get_latest())
+    st.markdown("Changes submitted")    
 
 elif commit_changes and user_message=="":
     st.error("Please add a source")
--- GABiP_GUI/pages/GABiP_Databases/csv_meta_data_db_creation.py
+++ GABiP_GUI/pages/GABiP_Databases/csv_meta_data_db_creation.py
@@ -10,16 +10,21 @@ deta_key=os.getenv("deta_key")
 #initialising a deta object
 deta_connection= Deta(deta_key)
 
-metaData=deta_connection.Base("database_versions")
+#metaData=deta_connection.Base("database_versions")
+metaData=deta_connection.Base("database_metadata")
 
-def insert_csv(date_time, file_Path, edit_type, username, status, value):
+#def insert_csv(date_time, file_Path, edit_type, username, status, value):
+#    """adding user"""
+#    #defining the email as the key
+#    return metaData.put({"key":date_time, "File_Path": file_Path, "Edit_Type": edit_type, "Edited_By":username, "Status":status, "Another_Column":value })
+def insert_csv(date_time, changes_file_Path, dataset_pre_change, edit_type, species_affected, genus_affected, username, user_comment, status, reason_denied, approved_by, date_approved, current_database_path):
     """adding user"""
     #defining the email as the key
-    return metaData.put({"key":date_time, "File_Path": file_Path, "Edit_Type": edit_type, "Edited_By":username, "Status":status, "Another_Column":value })
+    return metaData.put({"key":date_time, "Changes": changes_file_Path, "Dataset_Pre_Change": dataset_pre_change, "Edit_Type": edit_type, "Species_Affected": species_affected, "Genus_Affected": genus_affected,"Edited_By":username,"User_Comment": user_comment, "Status":status, "Reason_Denied":reason_denied, "Approved_By":approved_by, "Date_Approved":date_approved, "Current Dataset":current_database_path })
 
-
+#"C:/Users/Littl/OneDrive/Documents/GitHub/12528056_CSC7058/GABiP_GUI/pages/GABiP_Databases/testDB.csv"
 now=datetime.now()
-#insert_csv(str(now),"C:/Users/Littl/OneDrive/Documents/GitHub/12528056_CSC7058/GABiP_GUI/pages/GABiP_Databases/testDB.csv", "Deletion", "CCampbell", "Pending", "a value")
+#insert_csv(str(now), "n/a", "C:/Users/Littl/OneDrive/Documents/GitHub/12528056_CSC7058/GABiP_GUI/pages/GABiP_Databases/testDB.csv", "none", "n/a", "n/a", "admin", "n/a", "Approved", "n/a", "admin",str(now), "C:/Users/Littl/OneDrive/Documents/GitHub/12528056_CSC7058/GABiP_GUI/pages/GABiP_Databases/testDB.csv")
 
 #testing sorting method
 def get_all_paths():
@@ -31,7 +36,7 @@ databases=get_all_paths()
 
 date_time= sorted([database["key"] for database in databases], reverse=True)
 status=sorted([database["Status"] for database in databases])
-path = sorted([database["File_Path"] for database in databases])
+path = sorted([database["Current Dataset"] for database in databases])
 #print(status)
 #print(date_time)
 
@@ -50,7 +55,7 @@ def get_latest():
         #for database["key"] in databases:
       if database["key"]== i and database["Status"] =="Approved":
         break
-    return(database["File_Path"])
+    return(database["Current Dataset"])
 
 
 
