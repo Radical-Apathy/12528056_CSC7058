6f7642d708de63ddd7d851139d29360131ee1faf
Radical-Apathy <radical_apathy@outlook.com>
2023-01-11 11:02:21 +0000

    removing code duplication

--- GABiP_GUI/pages/02_Admin_Area.py
+++ GABiP_GUI/pages/02_Admin_Area.py
@@ -15,30 +15,6 @@ from datetime import datetime
 st.set_page_config(page_icon='amphibs.jpeg')
 
 #------------------------------------------------------------DATABASE CONNECTIONS-----------------------------------------------------------------------------------------#
-#------------------------------------------------------------USERS_DB DATABASE CONNECTION-----------------------------------------------------------------------------------------#
-load_dotenv("C:/Users/Littl/OneDrive/Documents/GitHub/12528056_CSC7058\GABiP_GUI/.env.txt")
-deta_key=os.getenv("deta_key")
-
-
-#initialising a deta object
-deta_connection= Deta(deta_key)
-
-users_db=deta_connection.Base("users_db")
-
-def get_all_users():
-    res = users_db.fetch()
-    #print(res.items) #using return here gives an address
-    return res.items
-
-#converts each individual values for users to a their own list using list comprehension
-users=get_all_users()
-email=[user["key"] for user in users]
-username=[user["username"] for user in users]
-firstname=[user["firstname"] for user in users]
-surname = [user["surname"] for user in users]
-hashed_passwords=[user ["password"] for user in users]
-isApproved=[user["approved"]for user in users]
-isAdmin=[user["admin"] for user in users]
 
 #------------------------------------------------------------USERS_DB DATABASE CONNECTION-----------------------------------------------------------------------------------------#
 load_dotenv("C:/Users/Littl/OneDrive/Documents/GitHub/12528056_CSC7058\GABiP_GUI/.env.txt")
