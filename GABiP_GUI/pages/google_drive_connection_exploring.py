95ced46d50654072062d58836c6a6f2660baa0d8
Radical-Apathy <radical_apathy@outlook.com>
2023-02-13 18:57:03 +0000

    general development

--- GABiP_GUI/pages/google_drive_connection_exploring.py
+++ GABiP_GUI/pages/google_drive_connection_exploring.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import google.auth
 from google.oauth2 import service_account
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
-from gspread_pandas import Spread,Client
+from googleapiclient.errors import HttpError
 from datetime import datetime
+from googleapiclient.http import MediaIoBaseUpload
 import io
 
 
@@ -42,7 +44,7 @@ file = request.execute()
 file_id = "1TJs2ykby1yxJvLcnGXdTduoLrtl7csMV"
 google_url = f"https://drive.google.com/uc?id={file_id}"
 file_folder_url="https://drive.google.com/file/d/1TJs2ykby1yxJvLcnGXdTduoLrtl7csMV/view?usp=sharing"
-folder_url="https://drive.google.com/drive/u/1/folders/1sXg0kEAHvRRmGTt-wq9BbMk_aAEhu1vN"
+folder_url="https://drive.google.com/drive/u/1/folders/1sXg0kEAHvRRmGTt-wq9BbMk_aAEhu1vN/"
 st.write("from google drive, csv must be unrestricted")
 folder_id="1sXg0kEAHvRRmGTt-wq9BbMk_aAEhu1vN"
 dataset=load_dataset(google_url)
@@ -60,42 +62,74 @@ create_new_csv=st.button("Create a new csv")
 # file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
 #----------------------------------------------------------------------------------------------------------------#
 now=datetime.now()
-version=now.strftime("%d.%m.%Y-%H.%M.%S")
-
-        #path_end = version
+version=now.strftime("%d-%m-%Y-%H-%M-%S")
 newPath=version+"pathtesting"+"-approved"+".csv"
 
+#-----------------------------------------------------------working but saves it locally first---------------------#
+# if create_new_csv:
+#     dataset.to_csv(newPath, index=False)
+#     file_metadata = {'name': newPath, 'parents': [folder_id], 'mimeType': 'application/vnd.ms-excel'}
+#     media = MediaFileUpload(newPath, mimetype='text/csv')
+#     file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+#     st.write("File saved, check google drive!")
+  #................................................................................................................#
 if create_new_csv:
-    # Save the DataFrame as a CSV file in Google Drive
-    st.write(newPath)
-    dataset.head()
-    dataset.to_csv(newPath, index=False)
-    file_metadata = {'name': newPath, 'parents': [folder_id], 'mimeType': 'application/vnd.ms-excel'}
-    media = MediaFileUpload(newPath, mimetype='text/csv')
-    file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    # write CSV content to bytes
+    csv_bytes = io.StringIO()
+    dataset.to_csv(csv_bytes, index=False)
+    csv_bytes = csv_bytes.getvalue().encode('utf-8')
     
-
+    # upload bytes to Google Drive
+    file_metadata = {'name': newPath, 'parents': [folder_id], 'mimeType': 'text/csv'}
+    media = MediaIoBaseUpload(io.BytesIO(csv_bytes), mimetype='text/csv', resumable=True)
+    file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
     st.write("File saved, check google drive!")
-  
-
-filesaved_url="https://drive.google.com/drive/u/1/folders/1sXg0kEAHvRRmGTt-wq9BbMk_aAEhu1vN/dataset_clean.csv"
-
-readtest=load_dataset(filesaved_url)
-st.write("Reading saved file from google drive")
-st.write(readtest)
 
 
 
 
 
 
+st.write("Using pydrive to upload csv") #ref https://towardsdatascience.com/how-to-manage-files-in-google-drive-with-python-d26471d91ecd
+
+
+# import pandas as pd
+# from pydrive.auth import GoogleAuth
+# from pydrive.drive import GoogleDrive
+
+# # Authenticate and create PyDrive client
+# gauth = GoogleAuth()
+# gauth.LocalWebserverAuth() # follow the authentication steps in the browser
+# drive = GoogleDrive(gauth)
 
 
 
+# # Convert DataFrame to CSV file in memory
+# csv_file = dataset.to_csv(index=False)
+
+# # Upload CSV file to Google Drive
+# file = drive.CreateFile({'title': newPath})
+# file.Upload() # Upload file to Google Drive
+# file.SetContentString(csv_file) # Set the content of the file to the CSV data
+# file.Upload() # Update the file with the CSV data
 
 
+st.write("Uploading without using pydrive")
 
+# directtogoogledrive=st.button("Create directly to drive")
 
+# if directtogoogledrive:
+# # Convert DataFrame to CSV file in memory
+#  csv_file = dataset.to_csv(index=False)
+ 
+#  try:
+#     # Upload CSV file to Google Drive
+#      file_metadata = {'name': newPath, 'parents': [folder_id]}
+#      media = googleapiclient.http.MediaIoBaseUpload(io.StringIO(csv_file), mimetype='text/csv')
+#      file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+#      st.write(f'File ID: {file.get("id")}')
+#  except HttpError as error:
+#      st.write(f'An error occurred: {error}')
 
 
 
