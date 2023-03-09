63c699e0a65dddb30fba80806a03fd49819e7b1e
Radical-Apathy <radical_apathy@outlook.com>
2023-03-09 11:09:09 +0000

    adding new credentials to investigate google drive

--- GABiP_GUI/pages/02_Admin_Area.py
+++ GABiP_GUI/pages/02_Admin_Area.py
@@ -122,7 +122,7 @@ def get_latest_file_id(latest_approved_ds):
 latest_id=get_latest_file_id(latest_approved_ds)
 
 
-#@st.cache_data
+@st.cache_data
 def load_latest():
     current_db = pd.read_csv(f"https://drive.google.com/uc?id={latest_id}", encoding= 'unicode_escape')#, low_memory=False)
     return current_db
