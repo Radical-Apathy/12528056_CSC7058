919ff493a6042f3026fc1f73dc5858fe9b067ceb
Radical-Apathy <radical_apathy@outlook.com>
2023-03-25 13:23:20 +0000

    creating a troubleshooting page
    Creating a temporary page to help with google drive trouble shooting and exploring storing csv as blob in the database and how feasible it is

---
+++ GABiP_GUI/pages/drive troubleshooting.py
@@ -0,0 +1,114 @@
+from re import search
+import streamlit as st
+import pandas as pd
+import numpy as np
+from deta import Deta
+import os
+import json
+from dotenv import load_dotenv
+from datetime import datetime
+from st_aggrid import AgGrid
+#from io import StringIO
+from PIL import Image
+from google.oauth2 import service_account
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+from googleapiclient.http import MediaIoBaseUpload
+import io
+import sys
+
+
+
+#------------------------------------------------------------GOOGLE DRIVE CONNECTION---------------------------------------------------------------------------------#
+# Use the client ID and secret to create an OAuth 2.0 flow
+creds = Credentials.from_authorized_user_info(st.secrets["gcp_drive_account"])
+
+# Access the user's Google Drive
+
+service = build("drive", "v3", credentials=creds)
+
+#------------------------------------------------------------DATABASE CONNECTION-----------------------------------------------------------------------------------------#
+load_dotenv("C:/Users/Littl/OneDrive/Documents/GitHub/12528056_CSC7058\GABiP_GUI/.env.txt")
+deta_key=os.getenv("deta_key")
+
+#initialising a deta object
+deta_connection= Deta(deta_key)
+
+database_metadata=deta_connection.Base("database_metadata")
+
+#------------------------------------------------------------metadata METHODS-----------------------------------------------------------------------------------------#
+
+#fetching info from the database
+def get_all_paths():
+    res = database_metadata.fetch()
+    return res.items
+
+#calling method and creating a list comprehension
+databases=get_all_paths()
+
+date_time= sorted([database["key"] for database in databases], reverse=True)
+status=sorted([database["Status"] for database in databases])
+paths = [database["Dataset_In_Use"] for database in databases]
+edit_type=[database["Edit_Type"] for database in databases]
+changes=[database["Changes"] for database in databases]
+
+
+approved=[]
+def get_approved():
+    for database in databases:
+        
+            #if database["Edit_Type"]=="New Species Addition" and database["Status"] =="Approved":
+                if database["Status"] =="Approved":
+                
+                 approved.append(database["key"])
+
+get_approved()
+
+approvedordered=sorted(approved,reverse=True)
+
+
+def get_latest_ds(key):
+    for database in databases:
+        if database["key"] ==key:
+            return database["Dataset_In_Use"]
+
+
+latest_approved_ds=get_latest_ds(approvedordered[0])
+
+folder_id="1sXg0kEAHvRRmGTt-wq9BbMk_aAEhu1vN"
+
+def get_latest_file_id(latest_approved_ds):
+     
+     results = service.files().list(q="mimeType!='application/vnd.google-apps.folder' and trashed=false and parents in '{0}'".format(folder_id), fields="nextPageToken, files(id, name)").execute()
+     items = results.get('files', [])
+
+     if not items:
+         st.write('No files found.')
+     else:
+        for item in items:
+             if item['name'] == latest_approved_ds:
+                 
+                 return item['id']
+
+
+
+latest_id=get_latest_file_id(latest_approved_ds)
+
+
+@st.cache_data
+def load_latest():
+    current_db = pd.read_csv(f"https://drive.google.com/uc?id={latest_id}", encoding= 'unicode_escape')#, low_memory=False)
+    return current_db
+
+
+def load_latest_not_cached():
+    current_db = pd.read_csv(f"https://drive.google.com/uc?id={latest_id}", encoding= 'unicode_escape')#, low_memory=False)
+    return current_db
+
+try:
+     current=load_latest()
+except:
+     
+     st.markdown(f'<p style="font-family:sans-serif; color:White; font-size: 30px;"><strong>***   Due to high traffic, page is temporarily unavailable. Please try again in 20 minutes. Time of error    ***</strong></p>', unsafe_allow_html=True)
+
