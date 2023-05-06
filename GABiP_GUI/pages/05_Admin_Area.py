79c46757b68ab3f00600e364df2955fb2991911f
Radical-Apathy <radical_apathy@outlook.com>
2023-05-06 15:31:17 +0100

    adding manual upload method

--- GABiP_GUI/pages/05_Admin_Area.py
+++ GABiP_GUI/pages/05_Admin_Area.py
@@ -180,6 +180,63 @@ def get_current_user(email):
 def update_user(username, updates):
     return users_db.update(updates, username)
 
+def manual_dataset_upload():
+ 
+   
+ #-------------------------------------------------------------MANUAL UPLOAD METHODS----------------------------------------------------------------------------------------------#
+  
+  now=datetime.now()
+  version=now.strftime("%d.%m.%Y-%H.%M.%S")
+  folder_id="1sXg0kEAHvRRmGTt-wq9BbMk_aAEhu1vN"          
+  newPath=version+"-"+st.session_state['username']+"-manual uploaded"+".csv"
+  date_now=str(now)
+  def create_new_updated_dataset_google():
+                newDataset=new_dataset
+                newDataset = newDataset.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+                with io.BytesIO() as csv_buffer:
+                    csv_string = newDataset.to_csv(index=False)
+                    csv_buffer.write(csv_string.encode('utf-8'))
+                    csv_buffer.seek(0)
+                    for chunk in pd.read_csv(csv_buffer, chunksize=1000):
+                        # process each chunk as needed
+                        pass
+    
+            # upload bytes to Google Drive
+                file_metadata = {'name': newPath, 'parents': [folder_id], 'mimeType': 'text/csv'}
+                media = MediaIoBaseUpload(io.BytesIO(csv_string.encode('utf-8')), mimetype='text/csv', resumable=True)
+                file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+
+  def add_new_dataset():
+            # updates = {"Status":"Approved", "Reason_Denied":"n/a", "Decided_By":st.session_state['username'], 
+            #            "Decision_Date":str(now), "Dataset_In_Use":newPath, "Dataset_Pre_Change":newPath }
+            metaData.put({"key":str(now), "Changes":"Manual upload", "Dataset_In_Use":newPath, "Dataset_Pre_Change":"n/a","Decided_By": st.session_state['username'], "Decision_Date":str(now),"Edit_Type": "Manual upload","Edited_By":st.session_state['username'], "Genus_Affected":"n/a","Reason_Denied": "n/a","Species_Affected": "n/a", "Status":"Approved", "User_Comment":"n/a","User_Images":"n/a","User_Sources":"n/a"} )
+ #-------------------------------------------------------------MANUAL UPLOAD UI---------------------------------------------------------#
+  manual_upload_option=st.checkbox("Manually Upload a dataset")
+  
+  if manual_upload_option:
+   st.warning("Uploading a dataset will over-ride the current most recent dataset...Are you sure you want to proceed?")
+   dataset_uploader=st.file_uploader("Choose a CSV file", type="csv")
+
+   if dataset_uploader is not None:
+    try:
+        # Get the first file uploader object from the list
+        #uploaded_file = dataset_uploader[0]
+        
+        # Read the CSV file into a DataFrame
+        new_dataset = pd.read_csv(dataset_uploader)
+       
+        # Display the DataFrame in a Streamlit table
+        st.write(new_dataset)
+        upload_dataset=st.button("Upload")
+    except Exception as e:
+        st.error(f'Error loading CSV file: {str(e)}')
+    if upload_dataset:
+         create_new_updated_dataset_google()
+         add_new_dataset()
+         st.write("Dataset uploaded!")
+
+   
+
 #gets and displays users pending approval
 def display_pending_users():
     st.markdown("***")
